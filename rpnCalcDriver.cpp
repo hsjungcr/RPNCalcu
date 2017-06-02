f72cc3ba1b4e8ca6c90e27eccab45b38394eb3c6
Han Jung <hsjungcr@gmail.com>
2017-06-02 10:18:32 -0700

    initial commit

---
+++ rpnCalcDriver.cpp
@@ -0,0 +1,35 @@
+//----------------------------------------------------------------------------
+// CalcDriver.cpp
+//
+// functions:  main(void)
+//----------------------------------------------------------------------------
+#include <iostream>
+#include <cstdlib>
+#include "RPNCalc.h"
+
+using namespace std;
+//----------------------------------------------------------------------------
+//	Function:		main()
+//	Title:			Driver for RPN Calculator
+//	Description:	This file contains function main()
+//					which creates and starts a calculator
+//	Programmer:		Han S. Jung
+//					Chi Cheuk Chow
+//					Huy Nguyen
+//	Version:		1.0
+//	Environment:	Intel Xeon PC 
+//					Software:   MS Windows 7 for execution; 
+//					Compiles under Microsoft Visual C++.Net 2012
+//	Calls:			CRPNCalc constructor
+//	Returns:		EXIT_SUCCESS  = successful 
+//	History Log:
+//					6/11/15  AT  completed version 1.0
+//----------------------------------------------------------------------------
+int main(void)
+{
+	using L_VALUE_CALC::CRPNCalc;
+	CRPNCalc myCalc;
+	cout << endl << "Press \"enter\" to continue";
+	cin.get();
+	return EXIT_SUCCESS;
+}
